d8f252dd381120340aab6145786b38b6735c1596
Persia39 <grand.antonov@yandex.ru>
2019-10-17 00:42:28 +0300

    size in mb

--- Test/Test/main.cpp
+++ Test/Test/main.cpp
@@ -1,9 +1,9 @@
 #include "siganture.h"
 int main(int argc, char *argv[])
 {
-	int size_block;
-	if (argc == 4)//size block in byte
-		size_block = atoi(argv[3]);
+	long int size_block;
+	if (argc == 4)//size block in mb
+		size_block = atoi(argv[3])* 1048576;
 	else
 		size_block = 1048576;
 	try {
